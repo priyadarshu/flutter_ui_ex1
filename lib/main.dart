61d6a5fb711f9bcbee5e3201e06d278ea52bb5e5
priyadarshu <priyadarshinichettiar@gmail.com>
2020-12-31 21:05:04 +0530

    New flutter project sample UI ex1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main()=>runApp(
-  MaterialApp{
+  MaterialApp(
     home:HomePage(),
-  }
+  );
 );
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
