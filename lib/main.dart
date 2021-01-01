f3e96b14f0650e821cff58dfa8ab888e885f89d0
priyadarshu <priyadarshinichettiar@gmail.com>
2021-01-01 17:27:49 +0530

    Added App Icons and deleted the default icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(
-  MaterialApp(
-    home:HomePage(),
-  );
-);
+void main() => runApp(
+      MaterialApp(
+        home: HomePage(),
+      ),
+    );
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,7 +15,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.blueGrey,
     );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
