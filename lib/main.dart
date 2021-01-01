f071d0adc95c6a9ed8f74af37e3eb51c5c872676
priyadarshu <priyadarshinichettiar@gmail.com>
2021-01-02 00:42:28 +0530

    Applied changes to adjust margin

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,9 @@ class _HomePageState extends State<HomePage> {
                       shrinkWrap: true,
                       children: [
                         Text('Iam text inside a List View'),
+                        SizedBox(
+                          height: 12,
+                        ),
                         Container(
                           child: CircleAvatar(
                             backgroundImage: AssetImage(
@@ -43,32 +46,8 @@ class _HomePageState extends State<HomePage> {
                             child: Text('Fishes'),
                           ),
                         ),
-                        Card(
-                          child: Container(
-                            alignment: Alignment.topLeft,
-                            padding: EdgeInsets.only(left: 12),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Sairam Chettiar',
-                                  textDirection: TextDirection.ltr,
-                                  style: TextStyle(
-                                    color: Colors.blueGrey,
-                                  ),
-                                ),
-                                Text(
-                                  'BE Mechanical Engineer',
-                                ),
-                                Text(
-                                  '5mins ago',
-                                  textDirection: TextDirection.rtl,
-                                ),
-                                Text('Hobbies : Trekking')
-                              ],
-                            ),
-                          ),
+                        SizedBox(
+                          height: 12,
                         ),
                         Card(
                           child: Container(
@@ -79,16 +58,49 @@ class _HomePageState extends State<HomePage> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                  'Sairam Chettiar',
+                                  'Eminem',
+                                  textDirection: TextDirection.ltr,
                                   style: TextStyle(
                                     color: Colors.blueGrey,
                                   ),
                                 ),
-                                Text('BE Mechanical Engineer'),
-                                Text('Hobbies : Trekking')
+                                Text(
+                                  'Singer,Rapper',
+                                ),
+                                Text(
+                                  'Online 5mins ago',
+                                  textDirection: TextDirection.rtl,
+                                ),
+                                Text('Hobbies : Singing, etc')
                               ],
                             ),
                           ),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Card(
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            padding: EdgeInsets.only(left: 12),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'The Rock',
+                                  style: TextStyle(
+                                    color: Colors.blueGrey,
+                                  ),
+                                ),
+                                Text(' Wrestler, Actor, Singer'),
+                                Text('Hobbies : Trekking, etc')
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 12,
                         ),
                       ],
                     ),
@@ -117,8 +129,8 @@ class NavUi extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Container(
-          margin: EdgeInsets.fromLTRB(42, 12, 42, 12),
-          width: 100.0,
+          margin: EdgeInsets.all(8),
+          width: 90.0,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -142,8 +154,8 @@ class NavUi extends StatelessWidget {
           ),
         ),
         Container(
-          margin: EdgeInsets.fromLTRB(42, 12, 42, 12),
-          width: 120.0,
+          margin: EdgeInsets.all(8),
+          width: 100.0,
           decoration: BoxDecoration(
             border: Border(
               left: BorderSide(color: Colors.black26, width: 2.0),
@@ -173,8 +185,8 @@ class NavUi extends StatelessWidget {
           ),
         ),
         Container(
-          margin: EdgeInsets.fromLTRB(42, 12, 42, 12),
-          width: 100.0,
+          margin: EdgeInsets.all(8),
+          width: 90.0,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
